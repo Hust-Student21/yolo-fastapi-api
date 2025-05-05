4f9a2fe033c5cb789393e11f7bd2f806081fdbab
Hust-Student21 <son.cvh.eng2110@gmail.com>
2025-05-05 15:21:33 +0700

    Initial YOLO API commit

---
+++ server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, UploadFile, File
+from ultralytics import YOLO
+import uvicorn
+from PIL import Image
+import io
+
+# Create FastAPI app
+app = FastAPI()
+
+# Load Model
+model = YOLO(r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\runs\train\100e_20_3\weights\100e203.pt')  # Change to your custom model if needed
+
+@app.get("/")  # Optional: homepage test route
+def read_root():
+    return {"message": "Server is alive."}
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    img = Image.open(io.BytesIO(contents))
+
+    results = model.predict(img)
+    return results[0].tojson()
